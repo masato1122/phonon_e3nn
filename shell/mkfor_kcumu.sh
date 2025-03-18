2b99fc7024d2b7f7a21a97a4bab659dbb789b0c8
masato1122 <masato.ohnishi.ac@gmail.com>
2025-03-19 03:05:18 +0900

    update

---
+++ shell/mkfor_kcumu.sh
@@ -0,0 +1,93 @@
+#!/bin/sh
+WORKDIR=$(cd $(dirname $0); pwd)
+
+#target=kspec_norm_freq; label="sp"
+target=kcumu_norm_mfp; label="cu"
+
+file_data=../../1_get/data_all5.csv
+
+if [ $# -ge 1 ]; then
+    id=$1
+else
+    echo -n "Input ID: "
+    read id
+fi
+
+num_epochs=1000
+nloop=1
+num_epochs_limit=1000
+patience=50
+
+###################
+gamma=0.95
+#
+batch=4
+weight_decay=0.00
+optimizer=adam
+###################
+
+for seed in `seq 1 20`; do
+for num_data in -1 3000 1000 300 100; do
+
+if [ $num_data -lt 1001 ]; then
+    nprocs=4
+else
+    nprocs=1
+fi
+
+############################################
+lr_init=0.01
+if [ $num_data == -1 ]; then
+    lr_init=`echo "5.0 / 6000." | bc -l`
+    lr_min=`echo "1.5 / 6000." | bc -l`
+else
+    lr_init=`echo "5.0 / $num_data" | bc -l`
+    lr_min=`echo "1.5 / $num_data" | bc -l`
+fi
+############################################
+
+jobname=j${label}${id}_${num_data}-${seed}
+
+OFILE=a.sh
+cat >$OFILE<<EOF
+#!/bin/sh
+#PBS -q workq
+#PBS -l nodes=1:ppn=${nprocs}
+#PBS -j oe
+#PBS -N $jobname
+
+export LANG=C
+export OMP_NUM_THREADS=${nprocs}
+cd \$PBS_O_WORKDIR
+rm $jobname.o*
+
+num_data=$num_data
+seed=${seed}
+outdir=./out_N\${num_data}/seed\${seed}
+
+for i in \`seq 1 $nloop\`; do
+    
+    python ../phonon_e3nn/tools/run_prediction.py \\
+        --file_data $file_data \\
+        --num_data  \$num_data \\
+        --outdir    \$outdir \\
+        --target    $target \\
+        --seed      \$seed \\
+        --r_max     4.3 \\
+        --num_epochs       $num_epochs \\
+        --num_epochs_limit $num_epochs_limit \\
+        --patience         $patience \\
+        --nprocs   ${nprocs} \\
+        --batch_size   $batch \\
+        --lr           $lr_init \\
+        --lr_min       $lr_min \\
+        --gamma        $gamma \\
+        --weight_decay $weight_decay \\
+        --optimizer    $optimizer
+    
+done
+EOF
+qsub $OFILE; sleep 0.1
+done
+done
+
